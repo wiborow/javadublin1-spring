0f7c6945b944b9902bb7ba2d15f8af5fbf2ec4f3
Wieslaw Borowski <wiborow@gmail.com>
2019-09-18 23:35:06 +0100

    init commit

---
+++ src/main/java/com/sda/javadublin1spring/Javadublin1SpringApplication.java
@@ -0,0 +1,13 @@
+package com.sda.javadublin1spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javadublin1SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javadublin1SpringApplication.class, args);
+    }
+
+}
