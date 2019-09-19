e9b7b4ed0137c39b052da6aaa269cbc0f8d97edf
Wieslaw Borowski <wiborow@gmail.com>
2019-09-19 19:33:23 +0100

    simple component and autowired

--- src/main/java/com/sda/javadublin1spring/Javadublin1SpringApplication.java
+++ src/main/java/com/sda/javadublin1spring/Javadublin1SpringApplication.java
@@ -1,13 +1,25 @@
 package com.sda.javadublin1spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class Javadublin1SpringApplication {
 
+    @Autowired
+    private HelloBean helloBean;
+
     public static void main(String[] args) {
+
         SpringApplication.run(Javadublin1SpringApplication.class, args);
+        System.out.println("blabla");
     }
 
+    @PostConstruct
+    public void run() {
+        helloBean.hello();
+    }
 }
